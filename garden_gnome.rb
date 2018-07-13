e16b0e1889ee1bb383c9b4204d3c2755b170b8fa
katesliang <kateliangg@gmail.com>
2018-07-13 17:01:29 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -6,13 +6,13 @@ class GardenGnome
     @personality = "evil"
     @hat_color = hat_color
   end
-def gnaw
+  def gnaw
   "Gnawing on a tree!!!"
   end
-def shout
+  def shout
   "GNARLY!!!"
-end
-def introduce_self
+  end
+  def introduce_self
 "Hello humans, my name is #{:name}, I am #{:age} years old, and you'll rue the day you crossed me!"
-end
+  end
 end
