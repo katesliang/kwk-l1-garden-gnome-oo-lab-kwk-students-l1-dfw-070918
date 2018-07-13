fd6e54375819bddbac84b4f61653c72dd2b10c84
katesliang <kateliangg@gmail.com>
2018-07-13 17:01:52 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -3,7 +3,7 @@ class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
   attr_reader :personality
   def intialize(hat_color)
-    @personality = "evil"
+    :personality = "evil"
     @hat_color = hat_color
   end
   def gnaw
