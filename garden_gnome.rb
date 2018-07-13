637093017240c50d8f84463b653bb00d1f6dfb13
katesliang <kateliangg@gmail.com>
2018-07-13 18:06:54 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -13,6 +13,6 @@ class GardenGnome
    "GNARLY!!!"
   end
   def introduce_self
-    "Hello humans, my name is #{:name}, I am #{:age} years old, and you'll rue the day you crossed me!"
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end
 end
